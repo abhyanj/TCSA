6753a20b232251a7f0297439caa746af88c097b0
Abhyan <>
2021-01-09 17:07:52 -0500

    modules

--- train.py
+++ train.py
@@ -1,1 +1,22 @@
 #training file
+import matplotlib
+matplotlib.use("Agg")
+# import the necessary packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import cv2
